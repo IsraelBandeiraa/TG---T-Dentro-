73feb70f8ae9af42d1372b3cf0f2aae6cf284b41
Israel Bandeira <87995747+IsraelBandeiraa@users.noreply.github.com>
2023-05-22 20:45:49 -0300

    Delete admin-app-fatec directory

--- admin-app-fatec/app.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-    console.log("ready!");
-    $("#navbar").load("header.html");
-
-    $('.carousel').carousel({
-        interval: 500
-
-    });
-});
--- admin-app-fatec/header.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-    console.log("ready!");
-});
-
-function sair() {
-    localStorage.setItem("usuariolide", "");
-    window.location.href = '/admin-app-fatec/login'
-}
